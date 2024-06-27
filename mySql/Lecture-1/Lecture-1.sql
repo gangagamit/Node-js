fa76d0e2fa406d2c9f7413cb7923cb79d522ac5b
gangagamit <gangagamit350@gmail.com>
2024-06-27 22:09:26 +0530

    27-6-2024

---
+++ mySql/Lecture-1/Lecture-1.sql
@@ -0,0 +1,24 @@
+-- starting:-
+-- 1.\sql --then enter
+-- connect sql
+-- 2.\connect root@localhost:3306
+
+-- 3.check database
+-- --show databases;
+
+-- 4.create database"-
+--  -- create database students;
+-- 5.delete
+-- 	--drop database studentes;
+-- 6.table use
+
+-- --use students;
+-- 7.create row and column
+
+-- create table user(firstname varchar(50),lastname(50), age int);
+-- 8.show table in database
+
+-- --show tables;
+
+-- 9.show student tables
+-- --describe user;
