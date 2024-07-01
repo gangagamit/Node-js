f26317707274e8a017aec0d457ed54722efdfe97
gangagamit <gangagamit350@gmail.com>
2024-07-01 08:29:20 +0530

    1-7-2024

--- mySql/Lecture-3/Lecture3.sql
+++ mySql/Lecture-3/Lecture3.sql
@@ -6,7 +6,17 @@ select * from productmaster where description = 'cottont jeans';
 select * from productmaster where sellprice > 350;
 select * from productmaster where sellprice < 350;
 
+select * from salemaster where city = 'Mumbai';
+select * from salemaster where TGTTOGET = 3000;
+update salemaster set city = 'Pune' where city = 'Mumbai';
+
+select * from clientmaster where city = 'Mumbai';
+update clientmaster set city = 'Mumbai' where city = 'Banglore';
+update clientmaster set city = 'Banglore' where clientNo = 'c0005';
+update clientmaster set Baldue = 1000 where clientNo = 'c0001';
 
 update productmaster set description = 'Tops' where  description = 'T-shirts';
 
 update productmaster set recordvl = 20 where description = 'skirts';
+
+update productmaster set costprice = 950 where description = 'trousers';
